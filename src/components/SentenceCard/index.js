220c10e2e19c1b7d10d3f7e13958ac6cb531f0bb
gerarjon <gerarsuaverdez@gmail.com>
2021-08-06 13:48:45 -0700

    input gets disabled when there is no answer to submit

--- src/components/SentenceCard/index.js
+++ src/components/SentenceCard/index.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './style.css'
 
-const SentenceCard = ({ currentQuestion, handleFormSubmit, handleAnswerOnChange, showAnswer, isCorrect, submittedAnswer }) => {
+const SentenceCard = ({ currentQuestion, handleFormSubmit, handleAnswerOnChange, showAnswer, isCorrect, submittedAnswer, }) => {
+  const [disabled, setDisabled] = useState(false)
+
+  useEffect(()=>{
+    if (currentQuestion) {
+      setDisabled(false)
+    } else {
+      setDisabled(true)
+    }
+  }, [currentQuestion])
+
   return(
     <div className="flashcard-submit-container columns is-multiline">
       <div className="flashcard-submit-question column is-full">
@@ -10,20 +20,21 @@ const SentenceCard = ({ currentQuestion, handleFormSubmit, handleAnswerOnChange,
       <div className="flashcard-submit-answer column is-full">
         {currentQuestion && showAnswer ? currentQuestion.answer : ''}
       </div>
-      
-      <form className="flashcard-submit-form column is-full" onSubmit={handleFormSubmit}>
-        <div className="field">
-          <label className="label">Translate to English</label>
-          <div className="control">
-            <input className={`input ${isCorrect === true ? "is-success" : `${isCorrect === false ? "is-danger" : ""}` }`} type="text" onChange={handleAnswerOnChange} value={submittedAnswer} placeholder="e.g. I am Takeshi" />
+      <fieldset disabled={disabled}>
+        <form className="flashcard-submit-form column" onSubmit={handleFormSubmit}>
+          <div className="field">
+            <label className="label">Translate to English</label>
+            <div className="control">
+              <input className={`input ${isCorrect === true ? "is-success" : `${isCorrect === false ? "is-danger" : ""}` }`} type="text" onChange={handleAnswerOnChange} value={submittedAnswer} placeholder="e.g. I am Takeshi" />
+            </div>
           </div>
-        </div>
-        <div className="field">
-          <div className="control">
-              <button className="button is-primary" type="submit">Submit</button>
+          <div className="field">
+            <div className="control">
+                <button className="button is-primary" type="submit">Submit</button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </fieldset>
     </div>
   )
 }
