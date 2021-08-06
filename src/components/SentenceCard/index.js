fa927f808a43f397f1667f41e6dd656f016292a2
gerarjon <gerarsuaverdez@gmail.com>
2021-08-06 02:46:58 -0700

    added visible question, answer, and answer submit form

--- src/components/SentenceCard/index.js
+++ src/components/SentenceCard/index.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import './style.css'
 
 const SentenceCard = ({ currentQuestion }) => {
   return(
-    <div>
-      {currentQuestion ? currentQuestion.question : ''}
+    <div className="flashcard-submit-container columns is-multiline">
+      <div className="flashcard-submit-question column is-full">
+        {currentQuestion ? currentQuestion.question : 'Loading...'}
+      </div>
+      <div className="flashcard-submit-answer column is-full">
+        {currentQuestion ? currentQuestion.answer : ''}
+      </div>
+      <div className="flashcard-submit-form column is-full">
+        <div className="field">
+          <label className="label">Translate in English</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="e.g. I am Takeshi" />
+          </div>
+        </div>
+        <div className="field">
+          <div className="control">
+              <button className="button is-primary">Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
